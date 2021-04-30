1c473edb8c40fd952a982e340e602721be610b3b
hmountainn <ghkdwltks123@naver.com>
2021-04-30 20:40:17 +0900

    popup 애니메이션 구현

--- friends/influence.js
+++ friends/influence.js
@@ -1,18 +1,37 @@
 window.addEventListener("load",function(){
-    var boxImg = document.querySelectorAll(".img");
-    var hideBox = document.querySelectorAll(".hide-box");
+    var bigbox = document.querySelector(".big-box");
+    var hideBox = bigbox.querySelector(".hide-box");
+    var minibox = bigbox.querySelector(".minibox");
+    var popup = bigbox.querySelector(".popup");
 
-    for(let i=0; i<boxImg.length; i++){
-        boxImg[i].onmouseover = function(e){
-            hideBox[i].classList.remove("d-none");
+    bigbox.onmouseover = function(e){
+        if(e.target.classList.contains("img")){
+            hideBox=e.target.parentElement.parentElement.querySelector(".hide-box")
+            hideBox.classList.remove("d-none");
             console.log("over");
         }
     }
 
-    for(let i=0; i<hideBox.length; i++){
-        hideBox[i].onmouseout = function(e){
-            hideBox[i].classList.add("d-none");
-            console.log("out");
+
+    bigbox.onmouseout = function(e){
+        if(e.target.classList.contains("hide-box")){
+            e.target.classList.add("d-none");
         }
     }
+
+    bigbox.onclick = function(e){
+        if(e.target.classList.contains("hide-box")){
+            var minipopup = e.target.parentElement.querySelector(".popup")
+            minipopup.classList.toggle("active");
+        }
+        if(e.target.classList.contains("close-btn")){
+            var minipopup = e.target.parentElement.parentElement
+            console.log(minipopup);
+            minipopup.classList.remove("active");
+        }
+        
+    }
+
+
+
 })
